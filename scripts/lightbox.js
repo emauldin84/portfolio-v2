feaa3e9f5017bcf5d6db3b0a9cd47fa72bd11940
Eric Mauldin <emauldin84@gmail.com>
2021-03-11 10:12:09 -0500

    adds protext screenshots and cleans up styling and js for the lightbox display. adjusts some copy.

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,6 +1,79 @@
+const protextImages = [
+  {
+    title: 'Login Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.07.29 AM.png",
+  },
+  {
+    title: 'Loan Dashboard Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.07.48 AM.png",
+  },
+  {
+    title: 'Loan General Tab',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.08.54 AM.png",
+  },
+  {
+    title: 'Loan Proceeds Tab',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.09.43 AM.png",
+  },
+  {
+    title: 'Review Loan Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.10.09 AM.png",
+  },
+  {
+    title: 'Loan Generating Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.10.34 AM.png",
+  },
+  {
+    title: 'Loan Errors Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.11.34 AM.png",
+  },
+  {
+    title: 'My Account Page',
+    imgSrc: "./images/protext/Screen Shot 2021-03-10 at 11.16.12 AM.png",
+  },
+]
+
+document.addEventListener('DOMContentLoaded', () => {
+  let protextCardThumbContainer = document.querySelector('#protext-thumb-container')
+  let protextCardThumbs = []
+
+  let protextModalThumbContainer = document.querySelector('#protext-modal-thumb-container')
+  let protextModalThumbs = []
+
+  let protextSlideContainer = document.querySelector('#protext-slide-container')
+  let protextModalSlides = []
+
+  protextImages.map((img, i) => {
+    console.log(img, i)
+    protextCardThumbs.push(
+      `<div class="img-column">
+        <img src="${img.imgSrc}" onclick="openModal();currentSlide(${i+1})" class="hover-shadow" alt="${img.title}">
+      </div>`
+    )
+    protextModalThumbs.push(
+      `<div class="img-column">
+        <img class="demo" src="${img.imgSrc}" onclick="currentSlide(${i+1})" alt="${img.title}">
+      </div>`
+    )
+    protextModalSlides.push(
+      `<div class="mySlides">
+        <div class="numbertext">${i+1} / ${protextImages.length}</div>
+        <img src="${img.imgSrc}" alt="${img.title}" style="width:100%">
+      </div>`
+    )
+
+  })
+
+  protextCardThumbContainer.innerHTML = protextCardThumbs.join('')
+  protextModalThumbContainer.innerHTML = protextModalThumbs.join('')
+  protextSlideContainer.insertAdjacentHTML('afterbegin', protextModalSlides.join(''))
+
+})
+
+
 // Open the Modal
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+  document.getElementById("myModal").style.display = "flex";
 }
 
 // Close the Modal
@@ -25,7 +98,6 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -34,7 +106,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex-1] ? slides[slideIndex-1].style.display = "block" : null;
+  dots[slideIndex-1] ? dots[slideIndex-1].className += " active" : null;
 }
